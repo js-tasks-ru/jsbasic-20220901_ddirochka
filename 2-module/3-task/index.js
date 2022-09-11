47e1efa472ed4ef55b372ae9af249bd4c3b42e96
Dmitriy Dirochka <dmitriy.dirochka@gmail.com>
2022-09-11 14:39:56 +0300

    added solution of the 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,41 +1,21 @@
-
-function read(a, b) { // Читает переменные и добавляет их в объет
-  calculator.variableA = a;
-  calculator.variableB = b;
-  return a, b;
-} 
-
-function sum(variables) {
-  let sumResult = 0;
-  for (let key in variables) {
-    if (Number.isFinite(variables[key])) {
-      sumResult += variables[key];
-    }
-  }
-  return sumResult;
-}
-
-function mul(variables) {
-  let mulResult = 1;
-  for (let key in variables) {
-    if (Number.isFinite(variables[key])) {
-      mulResult *= variables[key];
-    }
-  }
-  return mulResult;
-}
-
 let calculator = {
-  read: read,
-  sum: sum,
-  mul: mul,
+  read: function(a, b) {
+    this.a = a;
+    this.b = b;
+    return a, b;
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  },
 };
 
-
-calculator.read(7, 0);
+calculator.read(11, 13);
 console.log(calculator);
-console.log('Sum is ' + calculator.sum(calculator));
-console.log('Mul is ' + calculator.mul(calculator));
+console.log(calculator.sum());
+console.log(calculator.mul(calculator));
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-// window.calculator = calculator; // делает ваш калькулятор доступным глобально
+window.calculator = calculator; // делает ваш калькулятор доступным глобально
